9ba37e7b8b46700fb57f4cc21b279ce6fa27b3a6
James-Garofolo <garofo24@students.rowan.edu>
2024-04-16 17:18:38 -0400

    added vdp versions of a few models, need to push for rssm

---
+++ dreamerv2/models/dense_vdp.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.distributions as td
+import vdp
+
+class DenseModel(nn.Module):
+    def __init__(
+            self, 
+            output_shape,
+            input_size, 
+            info,
+            output_flag=False
+        ):
+        """
+        :param output_shape: tuple containing shape of expected output
+        :param input_size: size of input features
+        :param info: dict containing num of hidden layers, size of hidden layers, activation function, output distribution etc.
+        """
+        super().__init__()
+        self._output_shape = output_shape
+        self._input_size = input_size
+        self._layers = info['layers']
+        self._node_size = info['node_size']
+        self.activation = info['activation']
+        self.dist = info['dist']
+        self.model = self.build_model(output_flag)
+
+    def build_model(self, input_flag = False, output_flag=False):
+        model = [vdp.Linear(self._input_size, self._node_size, input_flag=True)]
+        model += [self.activation(tuple_input_flag=input_flag)]
+        for i in range(self._layers-1):
+            model += [vdp.Linear(self._node_size, self._node_size, tuple_input_flag=True)]
+            model += [self.activation(tuple_input_flag=True)]
+        model += [vdp.Linear(self._node_size, int(np.prod(self._output_shape)), output_flag=output_flag, tuple_input_flag=True)]
+        return nn.Sequential(*model)
+
+    def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
+        dist_mu, dist_sigma = self.model(mu_x, sigma_x)
+        if self.dist == 'normal':
+            return td.independent.Independent(td.Normal(dist_mu, dist_sigma), len(self._output_shape))
+        if self.dist == 'binary':
+            return td.independent.Independent(td.Bernoulli(logits=dist_mu), len(self._output_shape))
+        if self.dist == None:
+            return dist_mu, dist_sigma
+
+        raise NotImplementedError(self._dist)
--- dreamerv2/models/vdp.py
+++ dreamerv2/models/vdp.py
@@ -1,6 +1,6 @@
 import math
 import torch
-
+import torch.nn.functional as F
 
 def softplus(a):
     return torch.log(1.+torch.exp(torch.clamp(a, min=-88, max=88)))
@@ -11,16 +11,27 @@ def retrieve_elements_from_indices(tensor, indices):
     output = flattened_tensor.gather(dim=2, index=indices.flatten(start_dim=2)).view_as(indices)
     return output
 
+class Identity(torch.nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Identity, self).__init__(*args, **kwargs)
+
+    def forward(self, mu_x, sigma_x):
+        return mu_x, sigma_x
 
 class Linear(torch.nn.Module):
-    def __init__(self, in_features, out_features, bias=True, input_flag=False):
+    def __init__(self, in_features, out_features, bias=True, input_flag=False, output_flag=False, tuple_input_flag=False):
         super(Linear, self).__init__()
         self.input_flag = input_flag
+        self.tuple_input_flag = tuple_input_flag
         self.bias = bias
         self.mu = torch.nn.Linear(in_features, out_features, bias)
         self.sigma = torch.nn.Linear(in_features, out_features, bias)
-        torch.nn.init.xavier_normal_(self.mu.weight)
-        torch.nn.init.uniform_(self.sigma.weight, a=0, b=5)
+        if output_flag:
+            torch.nn.init.zeros_(self.mu.weight)
+            torch.nn.init.ones_(self.sigma.weight)
+        else:
+            torch.nn.init.xavier_normal_(self.mu.weight)
+            torch.nn.init.uniform_(self.sigma.weight, a=0, b=5)
 
     def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
         if self.input_flag:
@@ -28,6 +39,8 @@ class Linear(torch.nn.Module):
             sigma_y = mu_x ** 2 @ softplus(self.sigma.weight).T
             pass
         else:
+            if self.tuple_input_flag:
+                mu_x, sigma_x = mu_x
             mu_y = self.mu(mu_x)
             if len(sigma_x.shape) > 2:
                 sigma_y = (softplus(self.sigma.weight) @ sigma_x.mT).mT + \
@@ -39,7 +52,7 @@ class Linear(torch.nn.Module):
                         (mu_x ** 2 @ softplus(self.sigma.weight).T)
             pass
         if self.bias:
-            sigma_y += self.sigma.bias
+            sigma_y += softplus(self.sigma.bias)
         sigma_y *= 1e-3
         return mu_y, sigma_y
 
@@ -52,7 +65,7 @@ class Conv2d(torch.nn.Module):
     def __init__(self, in_channels, out_channels,
                  kernel_size=(5, 5), stride=1, padding=0, dilation=1,
                  groups=1, bias=True, padding_mode='zeros',
-                 input_flag=False):
+                 input_flag=False, tuple_input_flag=False):
         super(Conv2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -61,6 +74,7 @@ class Conv2d(torch.nn.Module):
         self.padding = padding
         self.dilation = dilation
         self.input_flag = input_flag
+        self.tuple_input_flag = tuple_input_flag
         self.mu = torch.nn.Conv2d(in_channels, out_channels,
                                   kernel_size, stride, padding, dilation,
                                   groups, bias, padding_mode)
@@ -78,6 +92,8 @@ class Conv2d(torch.nn.Module):
             sigma_y = sigma_y.view(mu_y.shape[0], mu_y.shape[1], mu_y.shape[2], mu_y.shape[3])
             pass
         else:
+            if self.tuple_input_flag:
+                mu_x, sigma_x = mu_x
             mu_y = self.mu(mu_x)
             vec_x = self.unfold(mu_x)
             sigma_y = (softplus(self.sigma.weight).repeat(1, vec_x.shape[1]) @ self.unfold(sigma_x)) + \
@@ -92,139 +108,140 @@ class Conv2d(torch.nn.Module):
                             - self.kernel_size - self.kernel_size * torch.log(softplus(self.sigma.weight)))
         return kl
     
-
-minvar = 100.0
-
-class Linear_minvar(torch.nn.Module):
-    def __init__(self, in_features, out_features, bias=True, input_flag=False):
-        super(Linear_minvar, self).__init__()
-        self.input_flag = input_flag
-        self.bias = bias
-        self.mu = torch.nn.Linear(in_features, out_features, bias)
-        self.sigma = torch.nn.Linear(in_features, out_features, bias)
-        torch.nn.init.xavier_normal_(self.mu.weight)
-        torch.nn.init.uniform_(self.sigma.weight, a=0, b=5)
-
-    def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
-        if self.input_flag:
-            mu_y = self.mu(mu_x)
-            sigma_y = mu_x ** 2 @ (softplus(self.sigma.weight).T + minvar)
-            pass
-        else:
-            mu_y = self.mu(mu_x)
-            if len(sigma_x.shape) > 2:
-                sigma_y = (softplus(self.sigma.weight) + minvar @ sigma_x.mT).mT + \
-                          (self.mu.weight**2 @ sigma_x.mT).mT + \
-                          (mu_x ** 2 @ (softplus(self.sigma.weight) + minvar).mT)
-            else:
-                sigma_y = ((softplus(self.sigma.weight) + minvar) @ sigma_x.T).T + \
-                        (self.mu.weight**2 @ sigma_x.T).T + \
-                        (mu_x ** 2 @ (softplus(self.sigma.weight) + minvar).T)
-            pass
-        if self.bias:
-            sigma_y += self.sigma.bias
-        sigma_y *= 1e-3
-        return mu_y, sigma_y
-
-    def kl_term(self):
-        kl = 0.5*torch.mean(self.mu.weight.shape[1] * (softplus(self.sigma.weight) + minvar) + torch.norm(self.mu.weight)**2
-                            - self.mu.weight.shape[1] - self.mu.weight.shape[1] * torch.log((softplus(self.sigma.weight) + minvar)))
-        return kl
-    
-class Conv2d_minvar(torch.nn.Module):
-    def __init__(self, in_channels, out_channels,
-                 kernel_size=(5, 5), stride=1, padding=0, dilation=1,
-                 groups=1, bias=True, padding_mode='zeros',
-                 input_flag=False):
-        super(Conv2d_minvar, self).__init__()
+class ConvTranspose2d(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=(5, 5), stride=1, 
+                 padding=0, output_padding=0, groups=1, bias=True, dilation=1, 
+                 padding_mode='zeros', input_flag=False, tuple_input_flag=False):
+        
+        super(ConvTranspose2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
+        self.output_padding = output_padding
         self.dilation = dilation
         self.input_flag = input_flag
-        self.mu = torch.nn.Conv2d(in_channels, out_channels,
-                                  kernel_size, stride, padding, dilation,
-                                  groups, bias, padding_mode)
+        self.tuple_input_flag = tuple_input_flag
+        self.mu = torch.nn.ConvTranspose2d(in_channels, out_channels,
+                                  kernel_size, stride, padding, output_padding,
+                                  groups, bias, dilation, padding_mode)
         torch.nn.init.xavier_normal_(self.mu.weight)
         self.sigma = torch.nn.Linear(1, out_channels, bias=bias)
         torch.nn.init.uniform_(self.sigma.weight, a=0, b=5)
 
         self.unfold = torch.nn.Unfold(kernel_size, dilation, padding, stride)
 
+    def vk(self):
+        """var kernel"""
+        if (type(self.kernel_size) == tuple) or (type(self.kernel_size) == list):
+            return self.sigma.weight.view(1,-1,1,1).repeat(self.in_channels, 1, 
+                                    self.kernel_size[0], self.kernel_size[1])
+        else:
+            return self.sigma.weight.view(1,-1,1,1).repeat(self.in_channels, 1, 
+                                    self.kernel_size, self.kernel_size)
+
     def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
         if self.input_flag:
             mu_y = self.mu(mu_x)
-            vec_x = self.unfold(mu_x)
-            sigma_y = (softplus(self.sigma.weight) + minvar).repeat(1, vec_x.shape[1]) @ vec_x ** 2
-            sigma_y = sigma_y.view(mu_y.shape[0], mu_y.shape[1], mu_y.shape[2], mu_y.shape[3])
+            #vec_x = self.unfold(mu_x)
+            #sigma_y = softplus(self.sigma.weight).repeat(1, vec_x.shape[1]) @ vec_x ** 2
+            #sigma_y = sigma_y.view(mu_y.shape[0], mu_y.shape[1], mu_y.shape[2], mu_y.shape[3])
+            sigma_y = F.conv_transpose2d(mu_x**2, softplus(self.vk()), None, self.stride,
+                                         self.padding, self.output_padding, 1, self.dilation)
             pass
         else:
+            if self.tuple_input_flag:
+                mu_x, sigma_x = mu_x
             mu_y = self.mu(mu_x)
-            vec_x = self.unfold(mu_x)
-            sigma_y = ((softplus(self.sigma.weight) + minvar).repeat(1, vec_x.shape[1]) @ self.unfold(sigma_x)) + \
-                      (self.mu.weight.view(self.out_channels, -1)**2 @ self.unfold(sigma_x).permute(2, 1, 0)).permute(2, 1, 0) + \
-                      ((softplus(self.sigma.weight) + minvar).repeat(1, vec_x.shape[1]) @ (vec_x ** 2).permute(2, 1, 0)).permute(2, 1, 0)
-            sigma_y = sigma_y.view(mu_y.shape[0], mu_y.shape[1], mu_y.shape[2], mu_y.shape[3])
+
+            sigma_y = F.conv_transpose2d(mu_x**2, softplus(self.vk()), None, self.stride,
+                                         self.padding, self.output_padding, 1, self.dilation) +\
+                      F.conv_transpose2d(sigma_x, self.mu.weight**2, None, self.stride,
+                                         self.padding, self.output_padding, 1, self.dilation) +\
+                      F.conv_transpose2d(sigma_x, softplus(self.vk()), None, self.stride,
+                                         self.padding, self.output_padding, 1, self.dilation)
+            #sigma_y = (softplus(self.sigma.weight).repeat(1, vec_x.shape[1]) @ self.unfold(sigma_x)) + \
+            #          (self.mu.weight.view(self.out_channels, -1)**2 @ self.unfold(sigma_x).permute(2, 1, 0)).permute(2, 1, 0) + \
+            #          (softplus(self.sigma.weight).repeat(1, vec_x.shape[1]) @ (vec_x ** 2).permute(2, 1, 0)).permute(2, 1, 0)
+            #sigma_y = sigma_y.view(mu_y.shape[0], mu_y.shape[1], mu_y.shape[2], mu_y.shape[3])
         sigma_y *= 1e-3
         return mu_y, sigma_y
 
     def kl_term(self):
-        kl = 0.5*torch.mean(self.kernel_size * (softplus(self.sigma.weight) + minvar) + torch.norm(self.mu.weight)**2
-                            - self.kernel_size - self.kernel_size * torch.log((softplus(self.sigma.weight) + minvar)))
+        if (type(self.kernel_size) == tuple) or (type(self.kernel_size) == list):
+            kl = 0.5*torch.mean(self.kernel_size[0] * softplus(self.sigma.weight) + 
+                torch.norm(self.mu.weight)**2 - self.kernel_size[0] - self.kernel_size[0] * torch.log(softplus(self.sigma.weight)))
+        else:
+            kl = 0.5*torch.mean(self.kernel_size * softplus(self.sigma.weight) + 
+                torch.norm(self.mu.weight)**2 - self.kernel_size - self.kernel_size * torch.log(softplus(self.sigma.weight)))
         return kl
 
 
 class MaxPool2d(torch.nn.Module):
     def __init__(self, kernel_size=2, stride=2, padding=0, dilation=1,
-                 return_indices=True, ceil_mode=False):
+                 return_indices=True, ceil_mode=False, tuple_input_flag=False):
         super(MaxPool2d, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.mu = torch.nn.MaxPool2d(kernel_size, stride, padding, dilation, return_indices, ceil_mode)
 
-    def forward(self, mu_x, sigma_x):
+    def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu_x, sigma_x = mu_x
         mu_y, where = self.mu(mu_x)
         sigma_y = retrieve_elements_from_indices(sigma_x, where)
         return mu_y, sigma_y
 
 
 class ReLU(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, tuple_input_flag=False):
         super(ReLU, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.relu = torch.nn.SELU()
 
-    def forward(self, mu, sigma):
+    def forward(self, mu, sigma=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu_a = self.relu(mu)
         sigma_a = sigma * (torch.autograd.grad(torch.sum(mu_a), mu, create_graph=True, retain_graph=True)[0]**2)
         return mu_a, sigma_a
     
 class GELU(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, tuple_input_flag=False):
         super(GELU, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.gelu = torch.nn.GELU()
 
     def forward(self, mu, sigma):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu_a = self.gelu(mu)
         sigma_a = sigma * (torch.autograd.grad(torch.sum(mu_a), mu, create_graph=True, retain_graph=True)[0]**2)
         return mu_a, sigma_a
     
 class Sigmoid(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, tuple_input_flag=False):
         super(Sigmoid, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.sigmoid = torch.nn.Sigmoid()
     
-    def forward(self, mu, sigma):
+    def forward(self, mu, sigma=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu_a = self.sigmoid(mu)
         sigma_a = sigma * (torch.autograd.grad(torch.sum(mu_a), mu, create_graph=True, retain_graph=True)[0]**2)
         return mu_a, sigma_a
 
 
 class Softmax(torch.nn.Module):
-    def __init__(self, dim=1):
+    def __init__(self, dim=1, tuple_input_flag=False):
         super(Softmax, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.softmax = torch.nn.Softmax(dim=dim)
 
-    def forward(self, mu, sigma):
+    def forward(self, mu, sigma=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu = self.softmax(mu)
         J = mu*(1-mu)
         sigma = (J**2) * sigma
@@ -232,11 +249,14 @@ class Softmax(torch.nn.Module):
 
 
 class BatchNorm2d(torch.nn.Module):
-    def __init__(self, num_features):
+    def __init__(self, num_features, tuple_input_flag=False):
         super(BatchNorm2d, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.mu = torch.nn.BatchNorm2d(num_features)
 
-    def forward(self, mu, sigma):
+    def forward(self, mu, sigma=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu_a = self.mu(mu)
         bn_param = ((self.mu.weight/(torch.sqrt(self.mu.running_var + self.mu.eps))) ** 2)
         sigma_a = bn_param[None, :, None, None] * sigma
@@ -244,11 +264,14 @@ class BatchNorm2d(torch.nn.Module):
     
 
 class LayerNorm(torch.nn.Module):
-    def __init__(self, normalized_shape):
+    def __init__(self, normalized_shape, tuple_input_flag=False):
         super(LayerNorm, self).__init__()
+        self.tuple_input_flag = tuple_input_flag
         self.mu = torch.nn.LayerNorm(normalized_shape)
 
-    def forward(self, mu, sigma):
+    def forward(self, mu, sigma=torch.tensor(0., requires_grad=True)):
+        if self.tuple_input_flag:
+            mu, sigma = mu
         mu_a = self.mu(mu)
         bn_param = ((self.mu.weight.unsqueeze(0)/(torch.sqrt(mu.var((-1), keepdim=True, unbiased=False) + self.mu.eps))) ** 2)
         sigma_a = bn_param * sigma
