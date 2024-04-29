45451235465aa8f3504d1d87b600f865bcc72644
James Garofolo <garofo24@students.rowan.edu>
2024-04-28 23:02:00 -0400

    added scores.png, going to make public

--- plot_results.py
+++ plot_results.py
@@ -3,5 +3,11 @@ import numpy as np
 
 baseline = np.loadtxt("avgs_baseline.csv")
 exp = np.loadtxt("avgs_exp.csv")
+t = np.arange(exp.shape[0])
+idx = exp.shape[0]-baseline.shape[0]
 
-print(baseline.shape, exp.shape)
+plt.plot(t[idx:], baseline, label="Dreamerv2")
+plt.plot(t, exp, label="Uncertainty Aware")
+plt.legend()
+plt.ylabel("average score")
+plt.show()
